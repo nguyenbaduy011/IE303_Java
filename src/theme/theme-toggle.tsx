be4b5774ef02a6f535a4718f9a33fd1fd32d15f2
nguyenbaduy011 <nguyenbaduy011@gmail.com>
2025-05-11 02:54:22 +0700

    style: update theme toggle UI

--- src/theme/theme-toggle.tsx
+++ src/theme/theme-toggle.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Moon, Sun } from "lucide-react";
+import { Moon, Sun, SunMoon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -11,8 +11,10 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
 
 export function ThemeToggle() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { theme, setTheme, resolvedTheme, systemTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
@@ -22,7 +24,6 @@ export function ThemeToggle() {
   }, []);
 
   if (!mounted) return null;
-
 
   return (
     <DropdownMenu>
@@ -37,13 +38,49 @@ export function ThemeToggle() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
+        <DropdownMenuItem
+          onClick={() => setTheme("light")}
+          className={cn(
+            "flex items-center gap-2 cursor-pointer",
+            theme === "light" && "font-medium"
+          )}
+        >
+          <Sun className="h-4 w-4" />
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
+        <DropdownMenuItem
+          onClick={() => setTheme("dark")}
+          className={cn(
+            "flex items-center gap-2 cursor-pointer",
+            theme === "dark" && "font-medium"
+          )}
+        >
+          <Moon className="h-4 w-4" />
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        <DropdownMenuItem
+          onClick={() => setTheme("system")}
+          className={cn(
+            "flex items-center gap-2 cursor-pointer",
+            (theme === "system" || !theme) && "font-medium"
+          )}
+        >
+          {/* <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-4 w-4"
+            aria-hidden="true"
+            role="img"
+          >
+            <title>System Theme Icon</title>
+            <path
+              fillRule="evenodd"
+              d="M3.5 2A1.5 1.5 0 002 3.5V15a3 3 0 003 3h12a1.5 1.5 0 001.5-1.5V3.5A1.5 1.5 0 0017 2H3.5zM5 5.75c0-.41.334-.75.75-.75h8.5a.75.75 0 010 1.5h-8.5a.75.75 0 01-.75-.75zM5.75 8.25a.75.75 0 00-.75.75v3.25c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75h-8.5z"
+              clipRule="evenodd"
+            />
+          </svg> */}
+          <SunMoon className="h-4 w-4" />
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
