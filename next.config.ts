924bd97f22593e462410e9dec9e72410ce262734
nguyenduy <nguyenbaduy011@gmail.com>
2025-05-25 09:47:57 +0700

    chore(config): enable Sass strict mode and set includePaths

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    strictmode: true,
+    includePaths: ["./app"],
+  },
 };
 
 export default nextConfig;
