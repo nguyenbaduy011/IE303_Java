02d1efe88a9f53f8d2bcb4bcec12efb23e94b97f
nguyenduy <nguyenbaduy011@gmail.com>
2025-05-25 11:40:07 +0700

    feat: replace mock user with authenticated user context in AppLayout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,30 +1,17 @@
+"use client";
+
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
+import { useAuth } from "@/contexts/auth-context";
 // import { SidebarNav } from "@/components/sidebar-nav";
-import { UserType } from "@/types/types";
-
-const mockUser: UserType = {
-  id: "12345",
-  first_name: "Joe",
-  last_name: "Mama",
-  email: "joemama@example.com",
-  birth_date: "1975-4-30",
-  image_url:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWii2rKTD3FgFmJVzm3Z3-QXdHOQhqm_7aWQZk_XoE4CfPByvrmH2cFshN4Trv5CPkxzs&usqp=CAU",
-  gender: "male",
-  nationality: "Vietnam",
-  phone_number: "+1234567890",
-  hire_date: "2022-01-01",
-  address: "123 Main Street, Springfield, IL",
-  created_at: "",
-  updated_at: "",
-};
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
+  const { user } = useAuth();
+
   return (
     <>
-      <Header user={mockUser} />
-      <main >
+      {user && <Header user={user} />}
+      <main>
         <div className="relative mx-auto  min-h-[calc(100vh-4rem)]">
           {/* <SidebarNav user={mockUser} isAdmin={true} className="absolute z-50"/> */}
 
