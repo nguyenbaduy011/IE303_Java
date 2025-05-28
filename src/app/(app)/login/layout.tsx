7f4a2cf880d1b448702e56e7ebb4e97182107607
nguyenduy <nguyenbaduy011@gmail.com>
2025-05-28 12:04:45 +0700

    refactor: remove container in login page

--- src/app/(app)/login/layout.tsx
+++ src/app/(app)/login/layout.tsx
@@ -4,10 +4,8 @@ export default function LoginLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="px-4 bg-slate-50">
-      <div className="container mx-auto px-4 min-h-[calc(100vh-4rem)]">
-        {children}
-      </div>
+    <main className="bg-slate-50">
+      <div className="mx-auto min-h-[calc(100vh-4rem)]">{children}</div>
     </main>
   );
 }
