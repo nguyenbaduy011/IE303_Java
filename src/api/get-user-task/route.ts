43ba5dffe8892419b0ab0a114acbddba3e53867b
nguyenbaduy011 <nguyenbaduy011@gmail.com>
2025-06-01 23:07:17 +0700

    refactor: update types response to use snake_case

--- src/api/get-user-task/route.ts
+++ src/api/get-user-task/route.ts
@@ -1,24 +1,24 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export type TaskType = {
   id: string;
-  createdAt: string;
-  updatedAt: string;
+  created_at: string;
+  updated_at: string;
   name: string;
   description: string;
   deadline: string;
   status: "pending" | "in_progress" | "completed" | "failed" | string;
-  assignedTo: {
+  assigned_to: {
     id: string;
-    firstName: string;
-    lastName: string;
+    first_name: string;
+    last_name: string;
   };
 }
 
 export interface TaskListResponse {
   tasks: TaskType[];
-  totalPages: number;
-  totalTaskCount: number;
-  totalElements: number;
+  total_pages: number;
+  total_task_count: number;
+  total_elements: number;
 }
 
 export const fetchTasks = async (userId: string): Promise<TaskListResponse> => {
@@ -40,33 +40,33 @@ export const fetchTasks = async (userId: string): Promise<TaskListResponse> => {
     const tasks: TaskType[] = Array.isArray(data.task)
       ? data.task.map((item: any) => ({
           id: item.id ?? "",
-          createdAt: item.createdAt ?? "",
-          updatedAt: item.updatedAt ?? "",
+          created_at: item.createdAt ?? "",
+          updated_at: item.updatedAt ?? "",
           name: item.name ?? "",
           description: item.description ?? "",
           deadline: item.deadline ?? "",
           status: item.status ?? "",
-          assignedTo: {
+          assigned_to: {
             id: item.assignedTo?.id ?? "",
-            firstName: item.assignedTo?.firstName ?? "",
-            lastName: item.assignedTo?.lastName ?? "",
+            first_name: item.assignedTo?.firstName ?? "",
+            last_name: item.assignedTo?.lastName ?? "",
           },
         }))
       : [];
 
     return {
       tasks: tasks,
-      totalPages: data.totalPages ?? 0,
-      totalTaskCount: data.totalTaskCount ?? 0,
-      totalElements: data.totalElements ?? 0,
+      total_pages: data.totalPages ?? 0,
+      total_task_count: data.totalTaskCount ?? 0,
+      total_elements: data.totalElements ?? 0,
     };
   } catch (error) {
     console.error("Error fetching tasks:", error);
     return {
       tasks: [],
-      totalPages: 0,
-      totalTaskCount: 0,
-      totalElements: 0,
+      total_pages: 0,
+      total_task_count: 0,
+      total_elements: 0,
     };
   }
 };
