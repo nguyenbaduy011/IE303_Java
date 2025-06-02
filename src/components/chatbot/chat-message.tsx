adc4c91993d9fb841d1ad1ae814bd3147b14967e
nguyenbaduy011 <nguyenbaduy011@gmail.com>
2025-06-02 20:09:45 +0700

    feat: add chatbot components

---
+++ src/components/chatbot/chat-message.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { useAutoScroll } from "@/hooks/use-auto-scroll";
+import { Message } from "ai";
+import { ArrowDown } from "lucide-react";
+
+
+export function ChatMessage()
