a420fe952f52e0425cff635c9811998a5d999087
nguyenduy <nguyenbaduy011@gmail.com>
2025-06-04 14:37:12 +0700

    feat: add chat messages component

--- src/components/chatbot/chat-message.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "@/components/ui/button";
-import { useAutoScroll } from "@/hooks/use-auto-scroll";
-import { Message } from "ai";
-import { ArrowDown } from "lucide-react";
-
-
-export function ChatMessage()
