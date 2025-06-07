c53a0a33586c2fc3f7a7e6fdf34913f8dd1c794c
nguyenduy <nguyenbaduy011@gmail.com>
2025-06-08 02:32:24 +0700

    feat: hide footer on chat page

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,25 +3,26 @@
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 import { useAuth } from "@/contexts/auth-context";
+import { usePathname } from "next/navigation";
 // import { SidebarNav } from "@/components/sidebar-nav";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   const { user } = useAuth();
+  const pathname = usePathname();
 
+  const shouldShowFooter = user && pathname !== "/chat";
   return (
     <>
       {/* {user?.passwordChangeRequired || (user && <Header user={user} />)} */}
       {user && <Header user={user} />}
 
       <main>
-        <div className="relative mx-auto  min-h-[calc(100vh-4rem)]">
-          {/* <SidebarNav user={mockUser} isAdmin={true} className="absolute z-50"/> */}
-
+        <div className="relative mx-auto min-h-[calc(100vh-4rem)]">
           {children}
         </div>
       </main>
       {/* {user?.passwordChangeRequired || (user && <Footer />)} */}
-      {user && <Footer />}
+      {shouldShowFooter && <Footer />}
     </>
   );
 }
