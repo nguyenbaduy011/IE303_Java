dbd4030d082708ccf123b8ddd310aa4cefbfc198
nguyenduy <nguyenbaduy011@gmail.com>
2025-06-11 00:12:34 +0700

    fix: hide footer and header when password requirement is true

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -10,18 +10,19 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   const { user } = useAuth();
   const pathname = usePathname();
 
-  const shouldShowFooter = user && pathname !== "/chat";
+  const shouldShowFooter =
+    user && pathname !== "/chat" && pathname !== "/change-password";
+  const shouldShowHeader = pathname !== "/change-password" && user;
+
   return (
     <>
-      {/* {user?.passwordChangeRequired || (user && <Header user={user} />)} */}
-      {user && <Header user={user} />}
+      {shouldShowHeader && user && <Header user={user} />}
 
       <main>
         <div className="relative mx-auto min-h-[calc(100vh-4rem)]">
           {children}
         </div>
       </main>
-      {/* {user?.passwordChangeRequired || (user && <Footer />)} */}
       {shouldShowFooter && <Footer />}
     </>
   );
